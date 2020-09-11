b802c8cc2d53d97858a6e60134eb9b76a1fbe0b7
Aio-Dev-JS <AioDev.JS@gmail.com>
2020-09-11 21:34:34 +0900

    Moive api

--- graphql/db.js
+++ graphql/db.js
@@ -1,16 +1,33 @@
-export const people = [
-  {
-    id: "1",
-    name: "aio",
-    age: 18,
-    gender: "male",
-  },
-  { id: "2", name: "aio1", age: 18, gender: "male" },
-  { id: "3", name: "aio2", age: 18, gender: "male" },
-  { id: "4", name: "aio3", age: 18, gender: "male" },
+let movies = [
+  { id: 0, name: "Moive 1", score: 3 },
+  { id: 1, name: "Moive 2", score: 5 },
+  { id: 2, name: "Moive 3", score: 7 },
+  { id: 3, name: "Moive 4", score: 1 },
 ];
 
+export const getMoives = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMoives = movies.filter((moive) => moive.id == id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id != id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: int(`${movies.length + 1}`),
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
